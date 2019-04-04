67e2e11d36df670aca675c999749d52fa31f6afc
Harry Heckmann <harry@MacBook-Pro-3.local>
2019-04-04 11:03:16 -0500

    Updates

---
+++ precache-manifest.c6e84f4247aedb109204dff9c1f688d2.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b4e2180603e059fad5bb47a4e816d21",
+    "url": "/static/media/abstract-orange.8b4e2180.jpg"
+  },
+  {
+    "revision": "831cb863bc9bab00ed47",
+    "url": "/static/css/main.337cee92.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b4b8ffa603719878f93d1afcf20cc658",
+    "url": "/static/media/github.b4b8ffa6.png"
+  },
+  {
+    "revision": "15c80e64cc7e9a81ed8c",
+    "url": "/static/js/2.1bbbf9db.chunk.js"
+  },
+  {
+    "revision": "703ff1a9d33c6155a8539f6368d9bf82",
+    "url": "/static/media/downward-rotation-white.703ff1a9.png"
+  },
+  {
+    "revision": "c30e1c7e97146a069a8c4003e35128db",
+    "url": "/static/media/downward-rotation.c30e1c7e.png"
+  },
+  {
+    "revision": "2b6c9b9b1e22162fb84f2f4617ea1a54",
+    "url": "/static/media/firebase.2b6c9b9b.png"
+  },
+  {
+    "revision": "da386a91e8bd90fe38cf41844238e162",
+    "url": "/static/media/attractive-black-white.da386a91.jpg"
+  },
+  {
+    "revision": "b683cdb02ad3eeabced7e67c141fda85",
+    "url": "/static/media/white-wall.b683cdb0.jpg"
+  },
+  {
+    "revision": "9beaa1d8238a9727360ea805b90cd39b",
+    "url": "/static/media/fern.9beaa1d8.jpg"
+  },
+  {
+    "revision": "328be0867765f9ac62fc18efb679099d",
+    "url": "/static/media/me_and_les.328be086.jpg"
+  },
+  {
+    "revision": "f251f4b14dd7a08869e4faaca06ec241",
+    "url": "/static/media/abstract-blue-3.f251f4b1.jpg"
+  },
+  {
+    "revision": "1d1c44570ee92cfbe34b16e014aa5c34",
+    "url": "/static/media/abstract-purple-2.1d1c4457.jpg"
+  },
+  {
+    "revision": "3cb6cf70259f3d7521bef8d3b05aac7c",
+    "url": "/static/media/dog.3cb6cf70.jpg"
+  },
+  {
+    "revision": "0fd3e673212a40a948bba25f9845fa91",
+    "url": "/static/media/update_bokeh.0fd3e673.jpg"
+  },
+  {
+    "revision": "831cb863bc9bab00ed47",
+    "url": "/static/js/main.fed7d93c.chunk.js"
+  },
+  {
+    "revision": "9c13e4d5fa4238514caa69ebade217b7",
+    "url": "/static/media/home.9c13e4d5.png"
+  },
+  {
+    "revision": "eb873ea1f7dbddbda5020f2b34d7114d",
+    "url": "/static/media/quiz_results.eb873ea1.png"
+  },
+  {
+    "revision": "6b9b1903ecc3cfb3b8e5031f6658c4eb",
+    "url": "/static/media/profile.6b9b1903.png"
+  },
+  {
+    "revision": "6e40652c0096f5050190b2d8bdbc6d98",
+    "url": "/static/media/search_results.6e40652c.png"
+  },
+  {
+    "revision": "dd9d7314f6bed28eea11f77a2342a78b",
+    "url": "/static/media/pet_info.dd9d7314.png"
+  },
+  {
+    "revision": "a1fe93ee344d93a0786ffa3ae5c6141a",
+    "url": "/static/media/landing.a1fe93ee.png"
+  },
+  {
+    "revision": "99ffc6fe656139655c12a526418d62ec",
+    "url": "/static/media/form.99ffc6fe.png"
+  },
+  {
+    "revision": "84d42ab5a030bf613c5a6cbde812a87b",
+    "url": "/static/media/search.84d42ab5.png"
+  },
+  {
+    "revision": "6f1d6a86c964946fb96005d8bc730362",
+    "url": "/static/media/search_results.6f1d6a86.png"
+  },
+  {
+    "revision": "6fe58e726fe84fd012473e90d94423cb",
+    "url": "/static/media/date.6fe58e72.png"
+  },
+  {
+    "revision": "03109d2d08d2d797c058d4018bae5ac6",
+    "url": "/static/media/profile.03109d2d.png"
+  },
+  {
+    "revision": "1962a971ea2262a318cb6893b5333703",
+    "url": "/static/media/abstract-multi.1962a971.jpg"
+  },
+  {
+    "revision": "fe6d47ad6a220d215aef3e8f085e0658",
+    "url": "/static/media/linkedin.fe6d47ad.png"
+  },
+  {
+    "revision": "ded9f0c015a5ea915049672884e5fedf",
+    "url": "/static/media/mail.ded9f0c0.png"
+  },
+  {
+    "revision": "15c80e64cc7e9a81ed8c",
+    "url": "/static/css/2.eea13b2b.chunk.css"
+  },
+  {
+    "revision": "9b14788187e3eb3d66547f9ea58a1968",
+    "url": "/index.html"
+  }
+];
--- src/Components/Projects/bestPet.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    bestPet: [
-        {title: 'Home', link: require('../../Photos/bestPet/home.png')},
-        {title: 'Quiz Results', link: require('../../Photos/bestPet/quiz_results.png')},
-        {title: 'Profile', link: require('../../Photos/bestPet/profile.png')},
-        {title: 'Search Results', link: require('../../Photos/bestPet/search_results.png')},
-        {title: 'Pet Info Page', link: require('../../Photos/bestPet/pet_info.png')}
-    ]
-}
